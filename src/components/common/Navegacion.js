8e84944d84d4eee3e97f770ba319a123dad10e16
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-05-18 17:58:30 -0300

    se agregó requisitos mínimos al navbar

--- src/components/common/Navegacion.js
+++ src/components/common/Navegacion.js
@@ -1,24 +1,52 @@
 import React from "react";
-import {Navbar, Nav} from 'react-bootstrap';
-import {NavLink} from 'react-router-dom';
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import logo from "../common/LogoRollingNews.png";
 
 const Navegacion = () => {
   return (
-    <Navbar bg="dark" expand="lg" variant='dark'>
-      <Navbar.Brand href="#home">RollingNews</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavLink exact={true} to='/' className='nav-link'>Inicio</NavLink>
-          <NavLink exact={true} to='' className='nav-link'>Economía</NavLink>
-          <NavLink exact={true} to='' className='nav-link'>Política</NavLink>
-          <NavLink exact={true} to='' className='nav-link'>Salud</NavLink>
-          <NavLink exact={true} to='' className='nav-link'>Tecnología</NavLink>
-          <NavLink exact={true} to='' className='nav-link'>Deportes</NavLink>
-          <NavLink exact={true} to='' className='nav-link'>Espectáculos</NavLink>
-          <NavLink exact={true} to='' className='nav-link'>Fotografía</NavLink>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img className="logo" src={logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink exact={true} to="/" className="nav-link">
+              Inicio
+            </NavLink>
+            <NavLink exact={true} to="" className="nav-link">
+              Actualidad
+            </NavLink>
+            <NavLink exact={true} to="" className="nav-link">
+              Espectáculos
+            </NavLink>
+            <NavLink exact={true} to="" className="nav-link">
+              Tecnología
+            </NavLink>
+            <NavLink exact={true} to="" className="nav-link">
+              Deportes
+            </NavLink>
+            {/* Otras categorías */}
+            <NavDropdown title="Categorías" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Salud</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Fotografía</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+              <NavLink exact={true} to="" className="nav-link ">
+                <strong className="margenIzq">Login</strong>
+              </NavLink>
+              <NavLink exact={true} to="" className="nav-link">
+                <strong>Suscripción</strong>
+              </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
