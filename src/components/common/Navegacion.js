0f69efd0727b392bdc790e3c3412655615149ed9
Emilia <emibteran@hotmail.com.ar>
2021-05-18 09:21:27 -0300

    se agregaron las secciones del diario al navbar

--- src/components/common/Navegacion.js
+++ src/components/common/Navegacion.js
@@ -4,12 +4,19 @@ import {NavLink} from 'react-router-dom';
 
 const Navegacion = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/">RollingNews</Navbar.Brand>
+    <Navbar bg="dark" expand="lg" variant='dark'>
+      <Navbar.Brand href="#home">RollingNews</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <NavLink exact={true} to='/' className="nav-link">Inicio</NavLink>
+          <NavLink exact={true} to='/' className='nav-link'>Inicio</NavLink>
+          <NavLink exact={true} to='' className='nav-link'>Economía</NavLink>
+          <NavLink exact={true} to='' className='nav-link'>Política</NavLink>
+          <NavLink exact={true} to='' className='nav-link'>Salud</NavLink>
+          <NavLink exact={true} to='' className='nav-link'>Tecnología</NavLink>
+          <NavLink exact={true} to='' className='nav-link'>Deportes</NavLink>
+          <NavLink exact={true} to='' className='nav-link'>Espectáculos</NavLink>
+          <NavLink exact={true} to='' className='nav-link'>Fotografía</NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
