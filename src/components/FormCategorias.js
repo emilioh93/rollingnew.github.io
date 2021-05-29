2929d855c9361b6f64f0cb7f9a273a8ae4def70d
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-05-29 15:44:49 -0300

    se trabajó en la lógica para agregar noticias y listarlas

--- src/components/FormCategorias.js
+++ src/components/FormCategorias.js
@@ -1,25 +1,106 @@
 import React from "react";
+import { useState } from "react";
 import { Container, Form, Button, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import Swal from "sweetalert2";
 
-const FormCategorias = () => {
+const FormCategorias = (props) => {
+  const [titulo, setTitulo] = useState("");
+  const [error, setError] = useState(false);
+  // Traer variable de entorno
+  const URL = process.env.REACT_APP_API_URL;
+  console.log(
+    "🚀 ~ file: FormCategorias.js ~ line 12 ~ FormCategorias ~ URL",
+    URL
+  );
+
+  // const leerCategoria = (e) => {
+  //   setCategoria(e.target.value);
+  // };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Validaciones
+    if (titulo.trim() !== "") {
+      // Si está todo ok, envío los datos del producto a la API
+      setError(false);
+
+      // Crear objeto
+      // const producto = {
+      //     nombreProducto: nombreProducto,
+      //     precioProducto: precioProducto,
+      //     categoria: categoria
+      // }
+      const categoria = {
+        titulo,
+      };
+      // Envío request POST
+      try {
+        // Estructura de datos a enviar
+        const cabecera = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(categoria),
+        };
+
+        const response = await fetch(URL, cabecera);
+        console.log(response);
+        if (response.status === 201) {
+          Swal.fire(
+            "Categoría agregada",
+            "La categoría se cargó correctamente",
+            "success"
+          );
+          // Reseteo form
+          e.target.reset();
+          // Actualizar datos
+          props.consultarAPI();
+          // Redireccionar al componente AdminNoticias
+        }
+      } catch (error) {
+        Swal.fire(
+          "Ocurrió un error",
+          "Inténtelo nuevamente en unos minutos",
+          "error"
+        );
+      }
+
+      // Espero respuesta
+    } else {
+      // Si no está todo ok, valido el error
+      setError(true);
+    }
+  };
+
   return (
     <Container className="my-5">
       <Link exact={true} to="/login/admin/categorias/" className="nav-link">
         <FontAwesomeIcon icon={faArrowLeft} className="me-2"></FontAwesomeIcon>
         Volver al administrador
       </Link>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <h3 className="my-5 text-center">Agregar Categoría</h3>
         <Form.Group>
           <Form.Label>Título</Form.Label>
           <Form.Control type="text"></Form.Control>
         </Form.Group>
-        <Button type="submit" className="mt-5 w-100 btn-success" size="lg" block>
+        <Button
+          type="submit"
+          className="mt-5 w-100 btn-success"
+          size="lg"
+          block
+        >
           Agregar categoría
         </Button>
+        {error === true ? (
+          <Alert variant="danger" className="my-5">
+            Faltan cargar datos obligatorios
+          </Alert>
+        ) : null}
       </Form>
     </Container>
   );
---
+++ src/components/ItemNoticia.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Container, ListGroup, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faPencilAlt,
+    faTrash,
+    faArrowLeft,
+  } from "@fortawesome/free-solid-svg-icons";
+import Swal from "sweetalert2";
+
+  
+
+const ItemNoticia = (props) => {
+    const eliminarNoticia = (codigo) => {
+        Swal.fire({
+          title: "¿Estás seguro de eliminar esta noticia?",
+          text: "No puedes recuperar una noticia que fue eliminada",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Eliminar",
+          cancelButtonText: "Cancelar",
+        }).then(async (result) => {
+          if (result.isConfirmed) {
+            // Agregar solicitud deleted
+            try {
+              const URL = process.env.REACT_APP_API_URL + "/" + codigo;
+              const respuesta = await fetch(URL, {
+                method: "DELETE",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              });
+    
+              console.log(respuesta);
+              if(respuesta.status === 200){
+                Swal.fire(
+                  "¡Noticia eliminada!",
+                  "La noticia seleccionada fue correctamente eliminada.",
+                  "success"
+                );
+                //  Volver a consultar la API
+                props.consultarAPI();  
+              }
+            } catch (error) {
+              console.log(error);
+              Swal.fire(
+                "Ocurrió un error!",
+                "Inténtelo nuevamente más tarde.",
+                "warning"
+              );
+            }
+            Swal.fire(
+              "¡Noticia eliminada!",
+              "La noticia seleccionada fue correctamente eliminada.",
+              "success"
+            );
+          }
+        });
+      };
+    
+
+  return (
+    <div>
+      <ListGroup.Item className="d-flex justify-content-between">
+        <p>
+          <span className="font-weight-bold">
+            Categoría: {props.noticia.categoria} - 
+          </span>
+          {props.noticia.titulo}{" "}
+        </p>
+        <div>
+          <Link
+            className="mr-2 btn btn-warning text-light"
+            to={`/productos/editar/${props.noticia.id}`}
+          >
+            <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>
+          </Link>
+          <Button
+            variant="danger"
+            onClick={() => eliminarNoticia(props.noticia.id)}
+          >
+            <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+          </Button>
+        </div>
+      </ListGroup.Item>
+    </div>
+  );
+};
+
+export default ItemNoticia;
