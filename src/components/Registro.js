3ec0c69b2ede583adc65eccd967125d8c8c77251
Pablo Helguera <phelguera33@gmail.com>
2021-05-30 20:29:05 -0300

    Commit de prueba

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -1,49 +1,59 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Container, Button } from "react-bootstrap";
 
-
 const Registro = () => {
-    const [usuarioCreado,setUsuarioCreado] = useState("")
-    const [passCreado,setPassCreado] = useState("")
-    
-    const listaCuentas = [];
+  const [usuarioCreado, setUsuarioCreado] = useState("");
+  const [passCreado, setPassCreado] = useState("");
+  const [cuenta, setCuenta] = useState("");
+  const [listaCuentas, setListaCuentas] = useState("");
 
-    
-    class Cuentas {
-        constructor( usuario, password){
-            this.usuario = usuario;
-            this.password = password;
-        }
+  class Cuentas {
+    constructor(usuario, password) {
+      this.usuario = usuario;
+      this.password = password;
     }
-    
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log('desde submit para registro');
-        
-        const nuevaCuenta = new Cuentas(usuarioCreado,passCreado);
-        console.log(nuevaCuenta);
+  }
 
-        listaCuentas.push(nuevaCuenta);
-        console.log(listaCuentas);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("desde formulario de cuentas");
+  
+  };
 
-        document.getElementById('registro').reset();
-
-    }
+  //crear cuenta con los valores del imput
+  const crearCuenta = ()=>{
+    const cuentaCreada = new Cuentas (usuarioCreado,passCreado)
+    console.log(cuentaCreada)
+    setCuenta(cuentaCreada);
+    console.log(cuenta)
+  }
+  
+  //agregar cuenta al arreglo de funciones
+  // const agregarCuenta = () => {
+  //   setListaCuentas(cuentaCreada)
+  //   console.log(listaCuentas)
+  // }
 
   return (
     <Container className="d-flex justify-content-center">
-      <Form className="my-3" onSubmit={handleSubmit} id='registro'>
+      <Form className="my-3" onSubmit={handleSubmit}  id="registro">
         <h1>Registrarse</h1>
         <Form.Group className="my-3">
           <Form.Label>Nombre de Usuario</Form.Label>
-          <Form.Control type="text" onChange={(e)=>setUsuarioCreado(e.target.value)}></Form.Control>
+          <Form.Control
+            type="text"
+            onChange={(e) => setUsuarioCreado(e.target.value)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group className="my-3">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control type="password" onChange={(e)=>setPassCreado(e.target.value)}></Form.Control>
+          <Form.Control
+            type="password"
+            onChange={(e) => setPassCreado(e.target.value)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group className="">
-          <Button className="" type="submit">
+          <Button className="" type="submit" onClick={crearCuenta}>
             Enviar
           </Button>
         </Form.Group>
