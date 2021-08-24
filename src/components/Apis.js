2bfc2eb0c51df8a3c7ef179294105724a414087e
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-08-24 13:11:20 -0300

    se corregió problemas de asincronía de api dólar

--- src/components/Apis.js
+++ src/components/Apis.js
@@ -3,16 +3,21 @@ import React from "react";
 const Apis = ({ dolar }) => {
   const dolarOficial =
     dolar && dolar.find((element) => element.casa.nombre === "Dolar Oficial");
-  
+
   const dolarBlue =
     dolar && dolar.find((element) => element.casa.nombre === "Dolar Blue");
-  
-    return (
+
+  return (
     <div className="d-flex px-5 py-3">
       <div className="me-auto">
         <strong>Dólar: </strong>
-        <span>{`BNA: $${dolarOficial.casa.compra}/$${dolarOficial.casa.venta}`}</span> |{" "}
-        <span>{`Blue: $${dolarBlue.casa.compra}/$${dolarBlue.casa.venta}`}</span>
+        <span>{`BNA: $${dolarOficial && dolarOficial.casa.compra}/$${
+          dolarOficial && dolarOficial.casa.venta
+        }`}</span>{" "}
+        |{" "}
+        <span>{`Blue: $${dolarBlue && dolarBlue.casa.compra}/$${
+          dolarBlue && dolarBlue.casa.venta
+        }`}</span>
       </div>
       <div>
         <span>Link</span>
