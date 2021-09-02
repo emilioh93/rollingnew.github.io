8ce8333a80ea5fa161f184427e6970b21a1b3c1a
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-09-01 21:16:14 -0300

    se corrigió botón para volver atrás al por medio de método goBack de useHistory

--- src/components/AdminNoticias.js
+++ src/components/AdminNoticias.js
@@ -1,21 +1,23 @@
 import React, { useEffect } from "react";
-import { Container, ListGroup } from "react-bootstrap";
+import { Button, Container, ListGroup } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import ItemNoticia from "./ItemNoticia";
 
 const AdminNoticias = ({ consultarAPI, noticias }) => {
+  let history = useHistory();
+
   useEffect(() => {
     consultarAPI();
     // eslint-disable-next-line
   }, []);
   return (
     <Container className="my-5">
-      <Link to="/login/admin" className="nav-link">
+      <Button onClick={() => history.goBack()} className="bg-light text-primary">
         <FontAwesomeIcon icon={faArrowLeft} className="me-2"></FontAwesomeIcon>
-        Volver al administrador
-      </Link>
+        Volver
+      </Button>
       <h2 className="text-center">Administrador de Noticias</h2>
       <div className="d-flex justify-content-end">
         <Link className="my-4 btn btn-primary" to="noticias/nueva">
