ad650a78c13ceed2eb620b27352b924f8d21380c
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-09-10 11:12:38 -0300

    se estilizaron noticias para que aparezcan en cards

--- src/components/Categoria.js
+++ src/components/Categoria.js
@@ -6,29 +6,31 @@ const Categoria = (props) => {
   return (
     <Container className="my-5 borde">
       <h2 className="my-4 text-center">{props.titulo}</h2>
-      {props.noticias.map((noticia, i) => {
-        return (
-          <Card key={i} className="mx-4 sombra">
-            <Card.Img variant="top" src={noticia.imgChica} />
-            <Card.Body>
-              <Card.Title className="text-center my-3">
-                {noticia.titulo}
-              </Card.Title>
-              <Card.Text className="text-center mt-4">
-                {noticia.resumen}
-              </Card.Text>
-            </Card.Body>
-            <div className="d-flex justify-content-center">
-              <Link
-                className="btn btn-primary my-3"
-                to={`/noticias/${noticia._id}`}
-              >
-                Leer más
-              </Link>
-            </div>
-          </Card>
-        );
-      })}
+      <div className="row d-flex justify-content-center">
+        {props.noticias.map((noticia, i) => {
+          return (
+            <Card key={i} className="col-sm-12 col-md-3 sombra m-2">
+              <Card.Img variant="top" src={noticia.imgChica} />
+              <Card.Body>
+                <Card.Title className="text-center my-3">
+                  {noticia.titulo}
+                </Card.Title>
+                <Card.Text className="text-center mt-4">
+                  {noticia.resumen}
+                </Card.Text>
+              </Card.Body>
+              <div className="d-flex justify-content-center">
+                <Link
+                  className="btn btn-primary my-3"
+                  to={`/noticias/${noticia._id}`}
+                >
+                  Leer más
+                </Link>
+              </div>
+            </Card>
+          );
+        })}
+      </div>
     </Container>
   );
 };
